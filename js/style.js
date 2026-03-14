51095cb5077acd681130fbd6b910793876615cc2
Supriya-004 <supriyawalhekar08@gmail.com>
2026-03-14 18:02:32 +0530

    css added

--- js/style.js
+++
@@ -1,8 +0,0 @@
-function openImage(img){
-    document.getElementById("preview").style.display="flex";
-    document.getElementById("previewImg").src=img.src;
-}
-
-function closeImage(){
-    document.getElementById("preview").style.display="none";
-}
